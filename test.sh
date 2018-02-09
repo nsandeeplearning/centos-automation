afe43e3aae44baad9289ed9839a62c9fe3511915
nsandeeppersonal <nsandeeppersonal@outlook.com>
2018-02-09 13:04:21 -0500

    test

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -
+#title           :pull_scripts_from_github.sh
+#description     :This script pull_scripts_from_github.sh
+#author          :nsandeep
+#date            :Fri Feb  9 06:07:04 EST 2018
+#version         :0.1
+#usage           :bash pull_scripts_from_github.sh
+
+date
