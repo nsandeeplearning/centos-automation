d0d640e4364e9b93add84f556f9ebe13fd59c822
nsandeeppersonal <nsandeeppersonal@outlook.com>
2018-02-09 13:43:08 -0500

    ok

--- oracle-instantclient12.2-tools-12-x64.sh
+++ oracle-instantclient12.2-tools-12-x64.sh
@@ -22,11 +22,3 @@ wget -O oracle-instantclient12.2-tools-12-x64.rpm  --no-cookies \
          --header "Cookie: oraclelicense=accept-securebackup-cookie" \
          -N http://ftp.riken.jp/Linux/cern/centos/7/cernonly/x86_64/Packages/oracle-instantclient12.2-tools-12.2.0.1.0-1.x86_64.rpm
 echo "Downloading oracle-instantclient12.2-tools-12.2.0.1.0-1.x86_64.rpm completed..."
-
-echo "Assiginging permissions to jdk-8-linux-x64.rpm started..."
-chmod 777 oracle-instantclient12.2-tools-12-x64.rpm
-echo "Assiginging permissions to jdk-8-linux-x64.rpm completed..."
-
-echo "oracle-instantclient12.2-tools-12-x64.rpm started......"
-oracle-instantclient12.2-tools-12-x64.rpm
-echo "oracle-instantclient12.2-tools-12-x64.rpm completed......"
