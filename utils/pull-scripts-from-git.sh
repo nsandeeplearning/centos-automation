aaff804c5be48a4982d9436a0931bb886563d497
snamin501 <snamin501@HQSWL-C027682.cable.comcast.com>
2018-02-25 18:08:06 -0500

    update

--- utils/pull-scripts-from-git.sh
+++ utils/pull-scripts-from-git.sh
@@ -1,10 +1,10 @@
 #!/bin/bash    
-#title           :pull files from github
-#description     :This scripts will pull files from git
-#author		     :nsandeep
-#date            :Fri Feb  9 06:07:04 EST 2018
-#version         :0.1
-# usage: pip install <pkg name>
+#title           :pull-scripts-from-git.sh
+#description     :This script will pull scripts from github 
+#author		     :sandeep(snamin501)
+#date            :Fri Feb  19 10:07:04 EST 2018
+#version         :1.0  
+#usage		     :sudo sh pull-scripts-from-git.sh
 
 if [[ $EUID -ne 0 ]]; then
    echo "your Not authorized to run  this script."
@@ -13,15 +13,23 @@ fi
 
 #githubURls:-
 install_url="https://raw.githubusercontent.com/nsandeeplearning/centos-automation/master/install"
-un_install_url="https://raw.githubusercontent.com/nsandeeplearning/centos-automation/master/uninstall"
 
 #softwares to download..
 inst_java_8="install-jdk-8-linux-x64.sh"
 inst_ora_client="install-oracle-instantclient12.2-tools-12-x64.sh"
 inst_py_devel="python-devel-2.7.5-58.el7.x86_64.rpm"
 inst_pip_and_wheels="install-pip-and-wheels.sh"
+inst_git_wget_zip="install-git_wget_zip.sh"
+inst_maven="install-maven.sh"
+inst_ms_sqlclient="install-ms-sqlclient.sh"
+inst_pentaho="install-pentaho.sh"
+pip_and_wheels="install-pip-and-wheels.sh"
+pip_packages="install-pip-packages.sh"
+install_python3="install-python3.sh"
+teradata_client_and_unixodbc="install-teradata-client-and-unixodbc.sh"
 
-declare -a softlist=($inst_java_8 $inst_ora_client $inst_py_devel $inst_pip_and_wheels)
+declare -a softlist=($inst_java_8 $inst_ora_client $inst_py_devel $inst_pip_and_wheels $inst_git_wget_zip
+$inst_maven $inst_ms_sqlclient $inst_pentaho $pip_and_wheels $pip_packages $install_python3 $teradata_client_and_unixodbc)
 
 #getting files from github.
 for soft in ${softlist[@]}
@@ -47,6 +55,7 @@ do
     dos2unix $soft
     echo "changing the document type $soft completed....."
 done
+
 
 
 
